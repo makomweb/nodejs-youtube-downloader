ad3ff7b73b03a68846af6f32c6c5dc9e4cad5068
Martin Komischke <martin.komischke@quentic.com>
2019-10-06 20:15:20 +0200

    update server code

--- server/index.js
+++ server/index.js
@@ -12,5 +12,9 @@ app.listen(4000, () => {
 
 app.get('/download', (req, res) => {
     var URL = req.query.URL;
-    res.json({ url: URL });
-})
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+
+    ytdl(URL, {
+        format: 'mp4'
+    }).pipe(res);
+});
